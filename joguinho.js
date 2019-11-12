581088d3cd4d83918e0f4e7352b5ec6c7608a3a8
Arthur <49170441+ArthurBMO@users.noreply.github.com>
2019-11-11 22:21:47 -0500

    Add files via upload

--- joguinho.js
+++ joguinho.js
@@ -1,16 +1,19 @@
-
-var x = 120;
+/*
+Equipe:
+  Arthur Beserra de Oliveira - 2A(Líder)
+  Suellen Medeiros Rodrigues - 2A
+  Etapa 4 e 5
+*/
 var y = 75;
 var xChoice = 240;
 var choice = 1;
 var telaAtiva;
-var starX;
-var starY;
-var star = [];
 var eliX = 0, eliY = 210;
-var cloudx = 100, cloudy = 100;
 var moonY=65, back=0, sunY=0;
-var perg = 2, resp=0;
+var perg = 1, resp=0;
+var gunshotY=560, ponto=0, inimigo=0;
+var ship1=150, ship2=150, ship3=150;
+var vida = 3;
 //let milliseconds = millis();
 
 function preload() {
@@ -22,15 +25,14 @@ function setup() {
 }
 
 function draw() {
+  menu();
+}
 
+function menu(){
   background(5,5,5);
-
-  //this is the moon
-  //ellipse(50, 70, 50, 50);
-  //this is the fast thinking name
+  //fo the name
   fill('#d0efff');
   textSize(50);
-  //text('Milliseconds \nrunning: \n' + milliseconds, 300, 200);
   text('Fast Thinking', 228.5, 90);
   //for the main text
   textSize(25);
@@ -43,12 +45,16 @@ function draw() {
     fill('white');
     text('Δ', 320, 260);
     text('Jogar', 350, 260);
+    if(keyCode == ENTER)
+      play();
   }
   if (y == 175) {
     choice = 2;
     fill('white');
     text('Δ', 310, 400)
     text('Regras', 340, 400);
+    if(keyCode == ENTER)
+      telaAtiva=2;
   }
   //some tips
   fill('white');
@@ -64,7 +70,6 @@ function draw() {
       rules();
       break;
   }
-
 }
 
 function play() {
@@ -72,6 +77,7 @@ function play() {
   if(back>=0&&back<=232){
     back+=40;
   }
+
   //moon
     ellipse(50, moonY+5, 50, 50);
   if(moonY>0&&sunY>=40){
@@ -100,48 +106,27 @@ function play() {
     eliY -= 1.5;
   }
 
-  //planes
+  fill('black');
+  textSize(20)
+  text('vidas: '+vida, 700, 50);
+
+  //enemies
   fill('white');
-  rect(200, 150, 50, 50)
+  rect(200, ship1, 50, 50)
   fill('black');
   text('plane', 200, 170);
 
   fill('white');
-  rect(300, 150, 50, 50)
+  rect(300, ship2, 50, 50)
   fill('black');
   text('plane', 300, 170);
 
   fill('white');
-  rect(400, 150, 50, 50)
+  rect(400, ship3, 50, 50)
   fill('black');
   text('plane', 400, 170);
 
-  //player
-  rect(150, 560, 10, 40);
-  rect(180, 560, 10, 40);
-
-  //questions
-  fill('orange');
-  rect(200, 300, 400, 200);
-  fill('black');
-  textSize(32);
-  text('Resolva a equação', 250, 330);
-  switch (respAtiva) {
-    case 1:
-      fill('orange');
-      rect(200, 300, 400, 200);
-      fill('black');
-      textSize(32);
-      text('Correta resposta, pressione enter', 222, 400);
-      break;
-    case 2:
-      fill('orange');
-      rect(200, 300, 400, 200);
-      fill('black');
-      textSize(32);
-      text('Errado, tome dano', 222, 400);
-  }
-  quiz();
+  quiz()
 }
 
 function rules() {
@@ -154,9 +139,48 @@ function rules() {
 }
 
 function quiz() {
+    switch (ponto) {
+      case 1:
+        fill('black');
+        rect(200, gunshotY, 10, 40);
+        if(gunshotY >=200){
+            gunshotY-=5;
+        }
+        if(gunshotY == 200){
+            ship1=0;
+        }
+        break;
+      case 2:
+        gunshotY=560;
+        fill('black');
+        rect(300, gunshotY, 10, 40);
+        if(gunshotY >=200){
+            gunshotY-=5;
+        }
+        if(gunshotY == 200){
+            ship2=0;
+        }
+        break;
+      case 3:
+        break;
+      default:
+
+    }
+
+    switch(inimigo){
+      case 1:
+        vida--;
+        break;
+    }
+
 
     switch(perg){
         case 1:
+            fill('orange');
+            rect(200, 300, 400, 200);
+            fill('black');
+            textSize(32);
+            text('Resolva a equação', 250, 330);
             text('10 x __ = 250', 296, 385);
             textSize(20);
             text('Quanto multiplicado por 10, dá 250?', 222, 420);
@@ -169,62 +193,40 @@ function quiz() {
                 resp=1;
                 fill('white');
                 text('Δ 25', 208, 470);
+                if(keyCode == ENTER){
+                    ponto++;
+                    perg=0;
+                }
             }
             if(xChoice==320){
                 resp=2;
                 fill('white');
                 text('Δ 50', 288, 470);
+                if(keyCode == ENTER){
+                  inimigo++;
+                }
             }
             if(xChoice==400){
                 resp=3
                 fill('white');
                 text('Δ 250', 368, 470);
+                if(keyCode == ENTER){
+                  inimigo=1;
+                }
             }
             if(xChoice==480){
                 resp=4
                 fill('white');
                 text('Δ 2,5', 468, 470);
-            }
-            break;
-        case 2:
-            text('4 x 7 = ___', 296, 385);
-            textSize(20);
-            text('Quatro multiplicado por sete, dá?', 222, 420);
-            textSize(32);
-            text('27', 240, 470);
-            text('28', 320, 470);
-            text('250', 400, 470);
-            text('25', 500, 470);
-            if(xChoice==240){
-                resp=1;
-                fill('white');
-                text('Δ 27', 208, 470);
-            }
-            if(xChoice==320){
-                resp=2;
-                fill('white');
-                text('Δ 28', 288, 470);
-            }
-            if(xChoice==400){
-                resp=3
-                fill('white');
-                text('Δ 250', 368, 470);
-            }
-            if(xChoice==480){
-                resp=4
-                fill('white');
-                text('Δ 25', 468, 470);
+                if(keyCode == ENTER){
+                  inimigo=1;
+                }
             }
             break;
     }
 }
 
 function keyPressed() {
-  //if enter is pressed, then main screen shows up
-  if (keyCode == ENTER) {
-    telaAtiva = choice;
-    respAtiva=resp;
-  }
   //selecting options
   if (keyCode === DOWN_ARROW) {
     if (y < 200)
@@ -233,12 +235,17 @@ function keyPressed() {
     if (y > 75)
       y -= 100;
   }
-  if (keyCode === RIGHT_ARROW){
-    if(xChoice<480)
-      xChoice += 80;
-  }else if (keyCode == LEFT_ARROW){
-    if(xChoice>240)
-      xChoice -= 80;
+  if(telaAtiva == 1 && keyCode == RIGHT_ARROW){
+      xChoice+=80;
+      if(xChoice > 480){
+          xChoice = 480;
+      }
+  }
+  else if(telaAtiva == 1 && keyCode == LEFT_ARROW){
+      xChoice-=80;
+      if(xChoice < 240){
+          xChoice = 240;
+      }
   }
   if(keyCode == ESCAPE && telaAtiva == 1){
       y = 75;
