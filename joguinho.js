fb76e363b650136125aab49b72838dcd12a6a7e4
Arthur <49170441+ArthurBMO@users.noreply.github.com>
2019-11-16 20:48:13 -0300

    Update joguinho.js

--- joguinho.js
+++ joguinho.js
@@ -8,19 +8,19 @@ Equipe:
 // 2 no x=tal e assim vai
 var xChoice = 240;
 //é pra aparecer a tela inicial(0), a do jogo(1) e as regras(2)
-var telaAtiva=0;
+var telaAtiva = 0;
 //serve pras montanhas subirem
 var eliX = 0, eliY = 210;
 //moonY é a posição da lua, sunY do sol e back pra o background sair do preto
 // pro branco. Tudo isso durante o jogo
-var moonY=65, back=0, sunY=0;
-var perg = 1, resp=0;
+var moonY = 65, back = 0, sunY = 0;
+var perg = 1, resp = 0;
 //gunshotY é a posição das balas, se for igual buga
-var gunshotY=560, gunshotY2=560, gunshotY3=560, ponto=0, inimigo=0;
+var gunshotY = 560, gunshotY2 = 560, gunshotY3 = 560, ponto = 0, inimigo = 0;
 //ship é a posição dos avião, pra eles poder sumir quando a bala chegar
-var ship1=150, ship2=150, ship3=150;
+var ship1 = 150, ship2 = 150, ship3 = 150;
 //se vida = 0, GAME OVER BITHC
-var vida = 3,tempo=0;
+var vida = 3, tempo = 0;
 
 
 
@@ -134,223 +134,223 @@ function play() {
 
 function quiz() {
   //aqui a cada ponto, a bala aparece pra explodir um aviao
-    switch (ponto) {
-      case 1:
-        fill('black');
-        rect(200, gunshotY, 10, 40);
-        //pra bala subir
-        if(gunshotY >=200){
-            gunshotY-=5;
-        }
-        //pra ela colidir com o aviao e puxar a proxima pergunta
-        if(gunshotY == 200){
-            ship1=-10;
-            gunshotY=-10;
-            perg=2;
-        }
-        break;
-      case 2:
-        fill('black');
-        rect(300, gunshotY2, 10, 40);
-        if(gunshotY2 >=200){
-            gunshotY2-=5;
-        }
-        if(gunshotY2 == 200){
-            ship2=-10;
-            gunshotY2=-10;
-            perg=3;
-        }
-        break;
-      case 3:
-        fill('black');
-        rect(400, gunshotY3, 10, 40);
-        if(gunshotY3 >=200){
-            gunshotY3-=5;
-        }
-        if(gunshotY3 == 200){
-            ship3=-10;
-            gunshotY3=-10;
-            perg=4;
-        }
-        break;
-    }
+  switch (ponto) {
+    case 1:
+      fill('black');
+      rect(200, gunshotY, 10, 40);
+      //pra bala subir
+      if (gunshotY >= 200) {
+        gunshotY -= 5;
+      }
+      //pra ela colidir com o aviao e puxar a proxima pergunta
+      if (gunshotY == 200) {
+        ship1 = -10;
+        gunshotY = -10;
+        perg = 2;
+      }
+      break;
+    case 2:
+      fill('black');
+      rect(300, gunshotY2, 10, 40);
+      if (gunshotY2 >= 200) {
+        gunshotY2 -= 5;
+      }
+      if (gunshotY2 == 200) {
+        ship2 = -10;
+        gunshotY2 = -10;
+        perg = 3;
+      }
+      break;
+    case 3:
+      fill('black');
+      rect(400, gunshotY3, 10, 40);
+      if (gunshotY3 >= 200) {
+        gunshotY3 -= 5;
+      }
+      if (gunshotY3 == 200) {
+        ship3 = -10;
+        gunshotY3 = -10;
+        perg = 4;
+      }
+      break;
+  }
 
-    //só pra testar mesmo
-    if(perg == 4){
-      telaAtiva=0;
-    }
+  //só pra testar mesmo
+  if (perg == 4) {
+    telaAtiva = 0;
+  }
 
-    switch(perg){
-        case 1:
-            //fundo das perguntas
-            fill('orange');
-            rect(200, 300, 400, 200);
-            fill('black');
-            textSize(32);
-            text('Resolva a equação', 250, 330);
-            //perguntas
-            text('10 x __ = 250', 296, 385);
-            textSize(20);
-            text('Quanto multiplicado por 10, dá 250?', 222, 420);
-            textSize(32);
-            //opções
-            text('25', 240, 470);
-            text('50', 320, 470);
-            text('250', 400, 470);
-            text('2,5', 500, 470);
-            //xChoice percorre as alterativa
-            if(xChoice==240){
-                resp=1;
-                fill('white');
-                text('Δ 25', 208, 470);
-                //90 e 122 é igual a z e Z, porque se for enter da bug
-                if(keyCode == 90 || keyCode == 122){
-                  //da um ponto e a pergunta vai 0, pra depois proceder
-                    ponto++;
-                    perg=0;
-                }
-            }
-            if(xChoice==320){
-                resp=2;
-                fill('white');
-                text('Δ 50', 288, 470);
-                //é pra perder vida msa ta bugando
-                if(keyCode == ENTER){
-                  inimigo++;
-                }
-            }
-            if(xChoice==400){
-                resp=3
-                fill('white');
-                text('Δ 250', 368, 470);
-                if(keyCode == ENTER){
-                  inimigo++;
-                }
-            }
-            if(xChoice==480){
-                resp=4
-                fill('white');
-                text('Δ 2,5', 468, 470);
-                if(keyCode == ENTER){
-                  inimigo++;
-                }
-            }
-            break;
-            case 2:
-                fill('orange');
-                rect(200, 300, 400, 200);
-                fill('black');
-                textSize(32);
-                text('Resolva a equação', 250, 330);
-                text('10 x __ = 250', 296, 385);
-                textSize(20);
-                text('Quanto multiplicado por 10, dá 250?', 222, 420);
-                textSize(32);
-                text('25', 240, 470);
-                text('50', 320, 470);
-                text('250', 400, 470);
-                text('2,5', 500, 470);
-                if(xChoice==240){
-                        resp=1;
-                        fill('white');
-                        text('Δ 25', 208, 470);
-                    }
-                    if(xChoice==320){
-                        resp=2;
-                        fill('white');
-                        text('Δ 50', 288, 470);
-                        if(keyCode == ENTER){
-                          inimigo++;
-                        }
-                    }
-                    if(xChoice==400){
-                        resp=3
-                        fill('white');
-                        text('Δ 250', 368, 470);
-                        if(keyCode == 90 || keyCode == 122){
-                            ponto++;
-                            perg=0;
-                        }
-                    }
-                    if(xChoice==480){
-                        resp=4
-                        fill('white');
-                        text('Δ 2,5', 468, 470);
-                        if(keyCode == ENTER){
-                          inimigo++;
-                        }
-            }
-            break;
-        case 3:
-            fill('orange');
-            rect(200, 300, 400, 200);
-            fill('black');
-            textSize(32);
-            text('Resolva a equação', 250, 330);
-            text('10 x __ = 250', 296, 385);
-            textSize(20);
-            text('Quanto multiplicado por 10, dá 250?', 222, 420);
-            textSize(32);
-            text('25', 240, 470);
-            text('50', 320, 470);
-            text('250', 400, 470);
-            text('2,5', 500, 470);
-            if(xChoice==240){
-                resp=1;
-                fill('white');
-                text('Δ 25', 208, 470);
-            }
-            if(xChoice==320){
-                resp=2;
-                fill('white');
-                text('Δ 50', 288, 470);
-                if(keyCode == ENTER){
-                  inimigo++;
-                }
-            }
-            if(xChoice==400){
-                resp=3
-                fill('white');
-                text('Δ 250', 368, 470);
-                if(keyCode == ENTER){
-                  inimigo++;
-                }
-            }
-            if(xChoice==480){
-                resp=4
-                fill('white');
-                text('Δ 2,5', 468, 470);
-                if(keyCode == 90 || keyCode == 122){
-                    ponto++;
-                    perg=0;
-                }
-            }
-            break;
-    }
+  switch (perg) {
+    case 1:
+      //fundo das perguntas
+      fill('orange');
+      rect(200, 300, 400, 200);
+      fill('black');
+      textSize(32);
+      text('Resolva a equação', 250, 330);
+      //perguntas
+      text('10 x __ = 250', 296, 385);
+      textSize(20);
+      text('Quanto multiplicado por 10, dá 250?', 222, 420);
+      textSize(32);
+      //opções
+      text('25', 240, 470);
+      text('50', 320, 470);
+      text('250', 400, 470);
+      text('2,5', 500, 470);
+      //xChoice percorre as alterativa
+      if (xChoice == 240) {
+        resp = 1;
+        fill('white');
+        text('Δ 25', 208, 470);
+        //90 e 122 é igual a z e Z, porque se for enter da bug
+        if (keyCode == 90 || keyCode == 122) {
+          //da um ponto e a pergunta vai 0, pra depois proceder
+          ponto++;
+          perg = 0;
+        }
+      }
+      if (xChoice == 320) {
+        resp = 2;
+        fill('white');
+        text('Δ 50', 288, 470);
+        //é pra perder vida msa ta bugando
+        if (keyCode == ENTER) {
+          inimigo++;
+        }
+      }
+      if (xChoice == 400) {
+        resp = 3
+        fill('white');
+        text('Δ 250', 368, 470);
+        if (keyCode == ENTER) {
+          inimigo++;
+        }
+      }
+      if (xChoice == 480) {
+        resp = 4
+        fill('white');
+        text('Δ 2,5', 468, 470);
+        if (keyCode == ENTER) {
+          inimigo++;
+        }
+      }
+      break;
+    case 2:
+      fill('orange');
+      rect(200, 300, 400, 200);
+      fill('black');
+      textSize(32);
+      text('Resolva a equação', 250, 330);
+      text('10 x __ = 250', 296, 385);
+      textSize(20);
+      text('Quanto multiplicado por 10, dá 250?', 222, 420);
+      textSize(32);
+      text('25', 240, 470);
+      text('50', 320, 470);
+      text('250', 400, 470);
+      text('2,5', 500, 470);
+      if (xChoice == 240) {
+        resp = 1;
+        fill('white');
+        text('Δ 25', 208, 470);
+      }
+      if (xChoice == 320) {
+        resp = 2;
+        fill('white');
+        text('Δ 50', 288, 470);
+        if (keyCode == ENTER) {
+          inimigo++;
+        }
+      }
+      if (xChoice == 400) {
+        resp = 3
+        fill('white');
+        text('Δ 250', 368, 470);
+        if (keyCode == 90 || keyCode == 122) {
+          ponto++;
+          perg = 0;
+        }
+      }
+      if (xChoice == 480) {
+        resp = 4
+        fill('white');
+        text('Δ 2,5', 468, 470);
+        if (keyCode == ENTER) {
+          inimigo++;
+        }
+      }
+      break;
+    case 3:
+      fill('orange');
+      rect(200, 300, 400, 200);
+      fill('black');
+      textSize(32);
+      text('Resolva a equação', 250, 330);
+      text('10 x __ = 250', 296, 385);
+      textSize(20);
+      text('Quanto multiplicado por 10, dá 250?', 222, 420);
+      textSize(32);
+      text('25', 240, 470);
+      text('50', 320, 470);
+      text('250', 400, 470);
+      text('2,5', 500, 470);
+      if (xChoice == 240) {
+        resp = 1;
+        fill('white');
+        text('Δ 25', 208, 470);
+      }
+      if (xChoice == 320) {
+        resp = 2;
+        fill('white');
+        text('Δ 50', 288, 470);
+        if (keyCode == ENTER) {
+          inimigo++;
+        }
+      }
+      if (xChoice == 400) {
+        resp = 3
+        fill('white');
+        text('Δ 250', 368, 470);
+        if (keyCode == ENTER) {
+          inimigo++;
+        }
+      }
+      if (xChoice == 480) {
+        resp = 4
+        fill('white');
+        text('Δ 2,5', 468, 470);
+        if (keyCode == 90 || keyCode == 122) {
+          ponto++;
+          perg = 0;
+        }
+      }
+      break;
+  }
 }
 
 function keyPressed() {
   //esses dois é pra percorrer as opções com o xChoice
-  if(telaAtiva == 1 && keyCode == RIGHT_ARROW){
-      xChoice+=80;
-      if(xChoice > 480){
-          xChoice = 480;
-      }
+  if (telaAtiva == 1 && keyCode == RIGHT_ARROW) {
+    xChoice += 80;
+    if (xChoice > 480) {
+      xChoice = 480;
+    }
   }
-  else if(telaAtiva == 1 && keyCode == LEFT_ARROW){
-      xChoice-=80;
-      if(xChoice < 240){
-          xChoice = 240;
-      }
+  else if (telaAtiva == 1 && keyCode == LEFT_ARROW) {
+    xChoice -= 80;
+    if (xChoice < 240) {
+      xChoice = 240;
+    }
   }
-  
+
   //ainda vou ajeitar esses dois
-  if(keyCode == ESCAPE && telaAtiva == 1){
-      y = 75;
-      telaAtiva=0;
-      back=0;
-      moonY=65;
-      draw();
+  if (keyCode == ESCAPE && telaAtiva == 1) {
+    y = 75;
+    telaAtiva = 0;
+    back = 0;
+    moonY = 65;
+    draw();
   }
   if (keyCode == ENTER && telaAtiva == 2) {
     telaAtiva = 0;
